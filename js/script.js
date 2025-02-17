07d66a10071d92c7b7b1330cb9f6b7b6c54625da
Lord Junn <58977552+LordJunn@users.noreply.github.com>
2025-02-17 11:35:31 +0800

    more comp net + js
    i should msg him

--- js/script.js
+++ js/script.js
@@ -134,15 +134,15 @@ function closeDisclaimer() {
 
 // Example usage
 const countDownDates = [
-    new Date("8 Feb, 2025 00:00:00").getTime(),
-    new Date("15 Feb, 2025 00:00:00").getTime(),
+    //new Date("8 Feb, 2025 00:00:00").getTime(),
+    new Date("25 Feb, 2025 09:00:00").getTime(),
     new Date("24 Mar, 2025 00:00:00").getTime(),
     new Date("29 Dec, 2024 16:56:00").getTime(),
     new Date("11 Jul, 2024, 09:00:00").getTime(),
 ];
 
 const countStartDates = [
-    new Date("4 Nov, 2024 00:00:00").getTime(),
+    //new Date("4 Nov, 2024 00:00:00").getTime(),
     new Date("4 Nov, 2024 00:00:00").getTime(),
     new Date("4 Nov, 2024 00:00:00").getTime(),
     new Date("29 Dec, 2024 16:55:00").getTime(),
@@ -150,7 +150,7 @@ const countStartDates = [
 ];
 
 const containerIds = [
-    "countdown-container-1",
+    //"countdown-container-1",
     "countdown-container-2",
     "countdown-container-3",
     "countdown-container-4",
