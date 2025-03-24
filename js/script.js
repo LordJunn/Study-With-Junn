5e5461992b17bb0aca9c4f0f35a644cc9f8f3651
Lord Junn <58977552+LordJunn@users.noreply.github.com>
2025-03-24 15:19:31 +0800

    oops forgotten about u

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ function closeDisclaimer() {
 
 // Example usage
 const countStartDates = [
-    new Date("4 Nov, 2024 00:00:00").getTime(),
+    //new Date("4 Nov, 2024 00:00:00").getTime(),
     new Date("24 Mar, 2025 00:00:00").getTime(),
     new Date("24 Mar, 2025 00:00:00").getTime(),
     new Date("29 Dec, 2024 16:55:00").getTime(),
@@ -142,7 +142,7 @@ const countStartDates = [
 ];
 
 const countDownDates = [
-    new Date("24 Mar, 2025 00:00:00").getTime(),
+    //new Date("24 Mar, 2025 00:00:00").getTime(),
     new Date("7 Apr, 2025 09:00:00").getTime(),
     new Date("29 Jun, 2025 00:00:00").getTime(),
     new Date("29 Dec, 2024 16:56:00").getTime(),
@@ -150,7 +150,7 @@ const countDownDates = [
 ];
 
 const containerIds = [
-    "countdown-container-1",
+    //"countdown-container-1",
     "countdown-container-2",
     "countdown-container-3",
     "countdown-container-4",
