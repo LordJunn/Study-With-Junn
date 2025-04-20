34506a05ad06b00d6dda9217e60a9d5b45d21fff
Lord Junn <58977552+LordJunn@users.noreply.github.com>
2025-04-20 12:21:14 +0800

    weekly update
    includes TDS 2201 as a whole
    along with minor updates here & there

--- js/script.js
+++ js/script.js
@@ -134,27 +134,15 @@ function closeDisclaimer() {
 
 // Example usage
 const countStartDates = [
-    //new Date("4 Nov, 2024 00:00:00").getTime(),
-    //new Date("24 Mar, 2025 00:00:00").getTime(),
     new Date("24 Mar, 2025 00:00:00").getTime(),
-    new Date("29 Dec, 2024 16:55:00").getTime(),
-    new Date("24 Jun, 2024, 00:00:00").getTime(),
 ];
 
 const countDownDates = [
-    //new Date("24 Mar, 2025 00:00:00").getTime(),
-    //new Date("7 Apr, 2025 09:00:00").getTime(),
     new Date("29 Jun, 2025 00:00:00").getTime(),
-    new Date("29 Dec, 2024 16:56:00").getTime(),
-    new Date("11 Jul, 2024, 09:00:00").getTime(),
 ];
 
 const containerIds = [
-    //"countdown-container-1",
-    //"countdown-container-2",
-    "countdown-container-3",
-    "countdown-container-4",
-    "countdown-container-5",
+    "countdown-container-1",
 ];
 
 initializeCountdowns(countDownDates, countStartDates, containerIds);
